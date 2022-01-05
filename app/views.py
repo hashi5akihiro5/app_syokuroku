a7bef792e443aae8e9eda941078969acba5793aa
akihiro.hashi <hashi5akihiro5@gmail.com>
2022-01-05 13:13:55 +0900

    1/5 Django version==3.2.11

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views import View
-
+#from filer.models import mixins
+#from .models import Schedule
 from .models import *
 from .forms import *
 
@@ -19,9 +20,9 @@ class IndexView(View):
         return render(request, 'app/index.html', self.params)
 
     def post(self, request):
-        # self.params['kcalselectform'] = KcalSelectForm(request.POST)
-        # self.params['form'] = MuneCreateForm(request.POST)
-        # self.params['formset'] = PostCreateFormSet(request.POST)
+        self.params['kcalselectform'] = KcalSelectForm(request.POST)
+        self.params['form'] = MuneCreateForm(request.POST)
+        self.params['formset'] = PostCreateFormSet(request.POST)
         return render(request, 'app/index.html', self.params)
 
 # Register
@@ -35,4 +36,16 @@ def register(request):
         'form': RegisterForm(),
     }
     return render(request, 'app/register.html', params)
+
+
+# MyCalendar
+# class MyCalendar(mixins.MonthCalendarMixin):
+#     model = Schedule
+#     date_filed = 'date'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         month_calendar_context = self.get_month_calendar()
+#         context.update(month_calendar_context)
+#         return context
 
